6cc123b7fcfb1a09966f0819c270aeb8446ebfb3
Howard Yeh <howard@metacircus.com>
2015-08-10 15:36:59 +0800

    basic layout

--- index.ios.js
+++ index.ios.js
@@ -11,39 +11,88 @@ var {
   Text,
   View,
   ScrollView,
+  Image,
+  TouchableOpacity,
 } = React;
 
-var Box = React.createClass({
+var CREATE_MEMO_ICON = require("image!createMemoIcon");
+
+var moment = require("moment");
+
+var Memo = React.createClass({
 
   onLayout: function(e) {
     var layout = e.nativeEvent.layout;
-    console.log(layout);
   },
 
   render: function() {
-    var  height = this.props.height || 200;
+    // var  height = this.props.height || 200;
+    var note = this.props.note;
     return (
-      <View style={[styles.box,{height: height}]}
+      <View style={[styles.memo]}
         onLayout={this.onLayout}>
+        <Text style={styles.memoText}>{note.text}</Text>
+        <Text style={styles.memoCreatedAt}>{moment(notes.createdAt).fromNow()}</Text>
       </View>
     );
   },
 });
 
-var Memo = React.createClass({
+var notes = [];
+
+var notesIDCounter = 0;
+function createNote(text) {
+  var id = notesIDCounter++;
+  notes.push({text: text, id: id, createdAt: new Date()});
+}
+
+createNote("You can't depend on your eyes when your imagination is out of focus.");
+createNote("All you need in this life is ignorance and confidence, and then success is sure.");
+createNote("Go to Heaven for the climate, Hell for the company.");
+createNote("If you tell the truth, you don't have to remember anything.");
+
+// TODO: prevent unblanced columns. I think one simple way to do is is to do a naive rendering pass (set opacity to 0).
+// Capture all the layout events to get the rendered dimensions of all the content.
+
+var AddMemoButton = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+
+  onLayout: function(e) {
+    var layout = e.nativeEvent.layout;
+    this.setState({width: layout.width});
+  },
+
   render: function() {
-    // Math.random() * 100 + 150
+    return (
+      <TouchableOpacity>
+        <View style={[styles.createMemoButton,{height: this.state.width}]} onLayout={this.onLayout}>
+          <Image source={CREATE_MEMO_ICON}/>
+        </View>
+      </TouchableOpacity>
+    )
+  },
+});
+
+var MemoList = React.createClass({
+  render: function() {
     var length = 10;
     var boxes1 = [];
     var boxes2 = [];
-    for (var i = 0; i < length; i++) {
-      var boxes = i % 2 == 0 ? boxes1 : boxes2;
+
+    boxes1.push(<AddMemoButton/>)
+
+    // var notes
+    for (var i = 0; i < notes.length; i++) {
+      var note = notes[i];
+      var boxes = i % 2 == 0 ? boxes2 : boxes1;
       var randHeight = Math.random() * 100 + 150;
-      boxes.push(<Box height={randHeight}/>);
+      boxes.push(<Memo id={note.id} note={note}/>);
     }
 
     return (
-      <ScrollView>
+      <ScrollView style={styles.scrollView}>
         <View style={styles.container}>
           <View style={[styles.column,{paddingLeft: 10}]}>
             {boxes1}
@@ -60,28 +109,48 @@ var Memo = React.createClass({
 });
 
 var styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: "#E8E8E8",
+  },
+
   container: {
     flex: 1,
     flexDirection: 'row',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#F5FCFF',
   },
 
   column: {
     flex: 1,
     flexDirection: 'column',
-    // margin: 5,
-    backgroundColor: 'rgba(255,0,0,0.4)',
+    justifyContent: 'flex-start',
   },
 
-  box: {
-    // width: 50,
-    height: 300,
-    backgroundColor: 'yellow',
+  createMemoButton: {
     marginRight: 10,
     marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    backgroundColor: '#F9F9F9',
+  },
+
+  memo: {
+    marginRight: 10,
+    marginBottom: 10,
+    padding: 5,
+
+    backgroundColor: '#F9F9F9',
+  },
+
+  memoText: {
+    fontSize: 18,
+    color: '#656565',
+  },
+
+  memoCreatedAt: {
+    marginTop: 18,
+    textAlign: 'right',
+    color: '#ACACAC',
   },
 });
 
-AppRegistry.registerComponent('Memo', () => Memo);
+AppRegistry.registerComponent('Memo', () => MemoList);
